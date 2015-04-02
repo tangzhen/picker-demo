06b00167fd6c663eb6f8484bd1cf9d0ddd27db7b
Zhen Tang <ztang@thoughtworks.com>
2015-04-02 23:18:09 +0800

    add picker to display makes.

--- pickerDemo/index.ios.js
+++ pickerDemo/index.ios.js
@@ -9,23 +9,44 @@ var {
   AppRegistry,
   StyleSheet,
   Text,
+  PickerIOS,
   View,
 } = React;
 
+var PickerItemIOS = PickerIOS.Item;
+
+var CAR_MAKES_AND_MODELS = {
+  amc: {
+    name: 'AMC',
+    models: ['AMX', 'Concord', 'Eagle', 'Gremlin', 'Matador', 'Pacer'],
+  },
+  alfa: {
+    name: 'Alfa-Romeo',
+    models: ['159', '4C', 'Alfasud', 'Brera', 'GTV6', 'Giulia', 'MiTo', 'Spider'],
+  }
+}
+
 var pickerDemo = React.createClass({
+  getInitialState: function() {
+    return {
+      carMake: 'alfa'
+    };
+  },
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
+        <Text>Please choose a make for your car:</Text>
+        <PickerIOS
+           selectedValue={this.state.carMake}>
+           {Object.keys(CAR_MAKES_AND_MODELS).map((carMake) => (
+            <PickerItemIOS
+              key={carMake}
+              value={carMake}
+              label={CAR_MAKES_AND_MODELS[carMake].name}
+              />
+            )
+          )}
+        </PickerIOS>
       </View>
     );
   }
@@ -35,19 +56,8 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('pickerDemo', () => pickerDemo);
